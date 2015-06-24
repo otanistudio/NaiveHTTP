5fb98ec1010f145b26181ee511548e58580fc95d
Robert Otani <robert@otanistudio.com>
2015-06-24 11:38:27 -0700

    Initial Commit

---
+++ NaiveHTTP/NaiveHTTP.h
@@ -0,0 +1,19 @@
+//
+//  NaiveHTTP.h
+//  NaiveHTTP
+//
+//  Created by Robert Otani on 6/24/15.
+//  Copyright © 2015 otanistudio.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NaiveHTTP.
+FOUNDATION_EXPORT double NaiveHTTPVersionNumber;
+
+//! Project version string for NaiveHTTP.
+FOUNDATION_EXPORT const unsigned char NaiveHTTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NaiveHTTP/PublicHeader.h>
+
+
