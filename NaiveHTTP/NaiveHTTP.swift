97c638df40a831fd1dccb821d5fadcf81720c4bc
Robert Otani <robert@otanistudio.com>
2015-06-24 17:13:49 -0700

    initial dumb implementation

--- NaiveHTTP/NaiveHTTP.swift
+++ NaiveHTTP/NaiveHTTP.swift
@@ -2,8 +2,84 @@
 //  NaiveHTTP.swift
 //  NaiveHTTP
 //
-//  Created by Robert Otani on 6/24/15.
+//  Created by Robert Otani on 6/20/15.
 //  Copyright © 2015 otanistudio.com. All rights reserved.
 //
 
 import Foundation
+import UIKit
+
+public class NaiveHTTP {
+    let urlSession: NSURLSession!
+    
+    required public init(configuration: NSURLSessionConfiguration?) {
+        if let config = configuration {
+            urlSession = NSURLSession(configuration: config)
+        } else {
+            urlSession = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        }
+    }
+    
+    deinit {
+        urlSession.invalidateAndCancel()
+    }
+    
+    public func imageGET(uri uri:String, success:((image: UIImage?)->())?, failure:(()->())?) {
+        
+        let url = NSURL(string: uri)!
+        let request = NSMutableURLRequest(URL: url)
+        request.setValue("image/png,image/jpg,image/jpeg,image/tiff,image/gif", forHTTPHeaderField: "Accept")
+        
+        urlSession.dataTaskWithRequest(request) { (imageData: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            let image = UIImage(data: imageData!)
+            success!(image: image)
+            
+            }!.resume()
+        
+    }
+    
+    public func dataGET(uri uri:String, success:((data: NSData)->())?, failure:(()->())?) {
+        let url = NSURL(string: uri)!
+        
+        urlSession.dataTaskWithURL(url) { (responseData: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            
+            if (error != nil) {
+                failure!()
+                return
+            }
+            
+            if (httpResponse.statusCode > 400) {
+                failure!()
+                return
+            }
+            
+            success!(data: responseData!)
+            
+            }!.resume()
+    }
+    
+    public func jsonGET(uri uri:String, success:((json: JSON)->())?, failure:(()->())?) {
+        self.dataGET(uri: uri, success: { (data) -> () in
+            let json = JSON(data: data)
+
+            if let error = json.error {
+                debugPrint(error)
+                failure!()
+                return
+            }
+            
+            success!(json: json)
+            /*
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments)
+                success!(json: json)
+            } catch {
+                failure!()
+            }
+            */
+            }, failure: failure)
+    }
+}
