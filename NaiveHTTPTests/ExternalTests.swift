695dc81b196c8352f265b085ae99f89400b85b76
Robert Otani <robert@otanistudio.com>
2015-06-24 17:14:07 -0700

    add a really dumb test

--- NaiveHTTPTests/ExternalTests.swift
+++ NaiveHTTPTests/ExternalTests.swift
@@ -12,24 +12,34 @@ class ExternalTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func testJSONGET() {
+        let naive = NaiveHTTP(configuration: nil)
+        let testURI = "http://otanistudio.com/feh/derp.json"
+        
+        let networkExpectation = self.expectationWithDescription("naive network expectation")
+        
+        naive.jsonGET(uri: testURI, success: { (json) -> () in
+            XCTAssertTrue(json.error == nil)
+            let expectedString = NSString(string: "{\"herp\":\"derp\"}")
+            let expectedData = expectedString.dataUsingEncoding(NSUTF8StringEncoding)
+            let expectedJSON = JSON(data: expectedData!)
+            XCTAssertEqual(expectedJSON, json)
+            networkExpectation.fulfill()
+            }) { () -> () in
+                XCTFail()
+                networkExpectation.fulfill()
+        }
+
+        self.waitForExpectationsWithTimeout(1.0) { (error) -> Void in
+            
         }
     }
+    
     
 }
