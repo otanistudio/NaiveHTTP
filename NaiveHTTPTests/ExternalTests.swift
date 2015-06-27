bf53cbfc6aa87d821b25dacc91f620925aceac6f
Robert Otani <robert@otanistudio.com>
2015-06-27 11:02:49 -0700

    fledgling tests

--- NaiveHTTPTests/ExternalTests.swift
+++ NaiveHTTPTests/ExternalTests.swift
@@ -20,22 +20,67 @@ class ExternalTests: XCTestCase {
     
     func testJSONGET() {
         let naive = NaiveHTTP(configuration: nil)
-        let testURI = "http://otanistudio.com/feh/derp.json"
+        let testURI = "https://httpbin.org/get?herp=derp"
         
         let networkExpectation = self.expectationWithDescription("naive network expectation")
         
         naive.jsonGET(uri: testURI, success: { (json) -> () in
-            XCTAssertTrue(json.error == nil)
-            let expectedString = NSString(string: "{\"herp\":\"derp\"}")
-            let expectedData = expectedString.dataUsingEncoding(NSUTF8StringEncoding)
-            let expectedJSON = JSON(data: expectedData!)
-            XCTAssertEqual(expectedJSON, json)
+            XCTAssertNil(json.error)
+            XCTAssertEqual("derp", json["args"]["herp"])
             networkExpectation.fulfill()
             }) { () -> () in
                 XCTFail()
                 networkExpectation.fulfill()
         }
         
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    func testBadImageGET() {
+        let naive = NaiveHTTP(configuration: nil)
+        
+        let networkExpectation = self.expectationWithDescription("naive network expectation")
+
+        naive.imageGET(uri: "http://httpbin.org/image/webp", success: { (image) -> () in
+            XCTAssertNil(image)
+            networkExpectation.fulfill()
+            }) { () -> () in
+                XCTFail()
+                networkExpectation.fulfill()
+        }
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    func testPNGImageGET() {
+        let naive = NaiveHTTP(configuration: nil)
+        
+        let networkExpectation = self.expectationWithDescription("naive network expectation")
+        
+        naive.imageGET(uri: "http://httpbin.org/image/png", success: { (image) -> () in
+            XCTAssertNotNil(image)
+            networkExpectation.fulfill()
+            }) { () -> () in
+                XCTFail()
+                networkExpectation.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1.0, handler: nil)
+    }
+    
+    func testJSONPOST() {
+        let naive = NaiveHTTP(configuration: nil)
+        let networkExpectation = self.expectationWithDescription("naive network expectation")
+        let postString = NSString(string: "{\"herp\":\"derp\"}")
+        let postData = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let postJSON = JSON(data: postData!)
+        naive.jsonPOST(uri: "https://httpbin.org/post", postData: postJSON, success: { (json) -> Void in
+            debugPrint(json)
+            XCTAssertEqual("derp", json["json"]["herp"])
+            networkExpectation.fulfill()
+            }) { () -> Void in
+                XCTFail()
+                networkExpectation.fulfill()
+        }
         self.waitForExpectationsWithTimeout(1.0, handler: nil)
     }
     
