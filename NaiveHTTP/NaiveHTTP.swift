398293421373030766e87bdad5ef3751ce0e3581
Robert Otani <robert@otanistudio.com>
2015-06-27 11:02:24 -0700

    add jsonPOST

--- NaiveHTTP/NaiveHTTP.swift
+++ NaiveHTTP/NaiveHTTP.swift
@@ -75,4 +75,35 @@ public class NaiveHTTP {
             
             }, failure: failure)
     }
+    
+    public func jsonPOST(uri uri:String, postData:JSON, success:( (json:JSON)->Void)?, failure:( ()->Void )?) {
+        let url = NSURL(string: uri)!
+        let request = NSMutableURLRequest(URL: url)
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPMethod = "POST"
+        
+        do {
+            try request.HTTPBody = postData.rawData()
+        } catch {
+            failure!()
+        }
+        
+        urlSession.dataTaskWithRequest(request) { (data, response, error) -> Void in
+            if error != nil {
+                failure!()
+                return
+            }
+            
+            let httpResponse = response as! NSHTTPURLResponse
+
+            if (httpResponse.statusCode > 400) {
+                failure!()
+                return
+            }
+            
+            let json = JSON(data: data!)
+            success!(json: json)
+        }!.resume()
+    }
 }
