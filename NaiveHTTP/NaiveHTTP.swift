393c7b7e9ac86afa5019ecf4524c4a79134f80e6
Robert Otani <robert@otanistudio.com>
2015-09-07 17:34:54 -0700

    move utility functions into their own extension

--- NaiveHTTP/NaiveHTTP.swift
+++ NaiveHTTP/NaiveHTTP.swift
@@ -51,6 +51,33 @@ public extension NaiveHTTPProtocol {
         
         return json!
     }
+    
+    public static func normalizedURL(uri:String, params:[String: String]?) -> NSURL {
+        // Deal with any query params already in the URI String
+        let urlComponents = NSURLComponents(string: uri)
+        var queryItems: [NSURLQueryItem]? = urlComponents?.queryItems
+        
+        if queryItems == nil {
+            queryItems = []
+        }
+        
+        // Now, incorporate items in queryParams to generate the fully-formed NSURL
+        if let p = params {
+            for (key, val) in p {
+                let qItem = NSURLQueryItem(name: key, value: val)
+                queryItems?.append(qItem)
+            }
+        }
+        
+        if queryItems!.count > 0 {
+            queryItems?.sortInPlace({ (qItem1: NSURLQueryItem, qItem2: NSURLQueryItem) -> Bool in
+                return qItem1.name < qItem2.name
+            })
+            urlComponents?.queryItems = queryItems
+        }
+        
+        return NSURL(string: (urlComponents?.string)!)!
+    }
 }
 
 public extension NaiveHTTPProtocol {
@@ -138,34 +165,6 @@ public class NaiveHTTP: NaiveHTTPProtocol {
         _urlSession.invalidateAndCancel()
     }
     
-    public class func normalizedURL(uri:String, params:[String: String]?) -> NSURL {
-        // Deal with any query params already in the URI String
-        let urlComponents = NSURLComponents(string: uri)
-        var queryItems: [NSURLQueryItem]? = urlComponents?.queryItems
-        
-        if queryItems == nil {
-            queryItems = []
-        }
-        
-        // Now, incorporate items in queryParams to generate the fully-formed NSURL
-        if let p = params {
-            for (key, val) in p {
-                let qItem = NSURLQueryItem(name: key, value: val)
-                queryItems?.append(qItem)
-            }
-        }
-        
-        if queryItems!.count > 0 {
-            queryItems?.sortInPlace({ (qItem1: NSURLQueryItem, qItem2: NSURLQueryItem) -> Bool in
-                return qItem1.name < qItem2.name
-            })
-            urlComponents?.queryItems = queryItems
-        }
-        
-        return NSURL(string: (urlComponents?.string)!)!
-    }
-
-
     public func GET(
         uri:String,
         params:[String: String]?,
