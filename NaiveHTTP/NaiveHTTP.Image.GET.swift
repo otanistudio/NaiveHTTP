a5fe0f09745f7144d8b1ab8d8d7e3cd303a1ea09
Robert Otani <robert@otanistudio.com>
2015-09-09 15:12:30 -0700

    refactor to use additional headers — and (ugh) should also use the actual request object that has the content-type

--- NaiveHTTP/NaiveHTTP.Image.GET.swift
+++ NaiveHTTP/NaiveHTTP.Image.GET.swift
@@ -14,9 +14,10 @@ public extension NaiveHTTPProtocol {
     func GET(uri:String, successImage:((image: UIImage?, response: NSURLResponse)->())?, failure:((error: NSError)->())?) {
         let url = NSURL(string: uri)!
         let request = NSMutableURLRequest(URL: url)
+        //TODO: (ASAP) We should be actually using this request object!!!
         request.setValue("image/png,image/jpg,image/jpeg,image/tiff,image/gif", forHTTPHeaderField: "Accept")
         
-        GET(uri, params: nil, success: { (imageData, response) -> () in
+        GET(uri, params: nil, additionalHeaders: nil, success: { (imageData, response) -> () in
             let image = UIImage(data: imageData)
             successImage!(image: image, response: response)
             }, failure: failure)
