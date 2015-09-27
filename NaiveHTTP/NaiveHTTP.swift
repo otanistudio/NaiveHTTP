2272b15bdacbbd032ea5697cc8c01ac68c2fd8e7
Robert Otani <robert@otanistudio.com>
2015-09-26 23:07:31 -0700

    move core implementation to reference type; mark class as final.

--- NaiveHTTP/NaiveHTTP.swift
+++ NaiveHTTP/NaiveHTTP.swift
@@ -32,7 +32,7 @@ public protocol NaiveHTTPProtocol {
     )
 }
 
-public class NaiveHTTP: NaiveHTTPProtocol {
+final public class NaiveHTTP: NaiveHTTPProtocol {
     let _urlSession: NSURLSession!
     let _configuration: NSURLSessionConfiguration!
     
@@ -56,5 +56,65 @@ public class NaiveHTTP: NaiveHTTPProtocol {
     
     deinit {
         _urlSession.invalidateAndCancel()
+    }
+    
+    public func performRequest(
+        method: Method,
+        uri: String,
+        body: AnyObject?,
+        headers: [String : String]?,
+        completion: completionHandler?) {
+            
+            let url = NSURL(string: uri)
+            let req = NSMutableURLRequest(URL: url!)
+            req.HTTPMethod = "\(method)"
+            
+            if headers != nil {
+                for (k, v) in headers! {
+                    req.setValue(v, forHTTPHeaderField: k)
+                }
+            }
+            
+            if method == .POST || method == .PUT {
+                if body != nil {
+                    do {
+                        let o = JSON(body!)
+                        if o.type == .String {
+                            req.HTTPBody = (o.stringValue as NSString).dataUsingEncoding(NSUTF8StringEncoding)
+                        } else {
+                            req.HTTPBody = try o.rawData()
+                        }
+                    } catch let jsonError as NSError {
+                        let bodyError = NSError(domain: errorDomain, code: -1,
+                            userInfo: [
+                                NSLocalizedFailureReasonErrorKey: "failed to convert body to appropriate format",
+                                NSLocalizedDescriptionKey: "SwiftyJSON Error: \(jsonError.description)"
+                            ])
+                        
+                        completion?(data: nil, response: nil, error: bodyError)
+                        return
+                    }
+                }
+            }
+            
+            
+            urlSession.dataTaskWithRequest(req) { (data, response, error) -> Void in
+                guard error == nil else {
+                    completion?(data: data, response: response, error: error)
+                    return
+                }
+                
+                if let httpResponse: NSHTTPURLResponse = response as? NSHTTPURLResponse {
+                    if (httpResponse.statusCode >= 400) {
+                        let responseError = NSError(domain: errorDomain, code: httpResponse.statusCode, userInfo: [NSLocalizedFailureReasonErrorKey: "HTTP 400 or above error", NSLocalizedDescriptionKey: "HTTP Error \(httpResponse.statusCode)"])
+                        completion?(data: data, response: response, error: responseError)
+                        return
+                    }
+                }
+                
+                completion?(data: data, response: response, error: error)
+                
+                }.resume()
+            
     }
 }
